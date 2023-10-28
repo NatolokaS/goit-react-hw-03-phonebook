5d82d954274dc5f06402e835f3ffa9f2d5f01c6a
NatolokaS <129997302+NatolokaS@users.noreply.github.com>
2023-10-29 01:19:00 +0300

    localStorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,36 @@
-import { Component } from 'react';
+import React, { Component } from 'react';
 import { PostForm } from './PostForm/PostForm';
 import { Filter } from './Filter/Filter';
 import { Contacts } from './Contacts/Contacts';
 import { Notify } from 'notiflix';
 
+const contactsData = [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+];
+
 export class App extends Component {
 
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: contactsData,
     filter: '',
   };
+
+  componentDidMount() {
+    const stringifiedContacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(stringifiedContacts) ?? contactsData;
+
+    this.setState({ contacts: parsedContacts });
+  }
+
+  componentDidUpdate(_, prevState) {
+    if(prevState.contacts !== this.state.contacts) {
+     const stringifiedContacts = JSON.stringify(this.state.contacts);
+     localStorage.setItem('contacts', stringifiedContacts);
+    }
+  }
 
   addContact  = (contact) => {
     const hasDuplicates = this.state.contacts.some(cont => cont.name === contact.name)
